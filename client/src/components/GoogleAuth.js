febbbddac122e772ae7b747cc7d6d82a13bdb17d
Rahul Rinwa <rahul.rinwah@gmail.com>
2022-01-17 23:25:40 +0530

    sending user into oAuth flow & updating auth state

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react'
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null }
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '777486060440-krdvv8qt61uqt98m2jq6qnpt813u8tpl.apps.googleusercontent.com',
-        scope: 'email',
-      })
+      window.gapi.client
+        .init({
+          clientId:
+            '777486060440-krdvv8qt61uqt98m2jq6qnpt813u8tpl.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance() //this.auth will give reference to the instance that we can use to sign in or sign out a user or get users current authentication status
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+          this.auth.isSignedIn.listen(this.onAuthChange)
+        })
     })
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+  }
+
+  onSignIn = () => {
+    this.auth.signIn()
+  }
+
+  onSignOut = () => {
+    this.auth.signOut()
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null
+    } else if (this.state.isSignedIn) {
+      return (
+        <button className='ui red google button' onClick={this.onSignOut}>
+          <i className='google icon' />
+          Sign Out
+        </button>
+      )
+    } else {
+      return (
+        <button className='ui red google button' onClick={this.onSignIn}>
+          <i className='google icon' />
+          Sign In with Google
+        </button>
+      )
+    }
+  }
+
   render() {
-    return (
-      <div>
-        <h1>Google Auth</h1>
-      </div>
-    )
+    return <div>{this.renderAuthButton()}</div>
   }
 }
 
